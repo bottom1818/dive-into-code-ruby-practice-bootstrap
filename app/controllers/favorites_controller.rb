c71595850b3ad9a2306b371e52df23ca6974ac64
Yoshimasa Shibata <shibata@nttpc.co.jp>
2018-03-18 10:08:26 +0000

    For Favorite Exam2

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,11 +6,13 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.create(blog_id: params[:blog_id])
     logger.debug("Debug favorite create-----")
     logger.debug(favorite)
-    redirect_to blogs_url, notice: "#{favorite.blog.user.name}さんのブログをお気に入り登録しました"
+    #redirect_to blogs_url, notice: "#{favorite.blog.user.name}さんのブログをお気に入り登録しました"
+    redirect_to blogs_url
   end
 
   def destroy
     favorite = current_user.favorites.find_by(blog_id: params[:blog_id]).destroy
-    redirect_to blogs_url, notice: "#{favorite.blog.user.name}さんのブログをお気に入り解除しました"
+    #redirect_to blogs_url, notice: "#{favorite.blog.user.name}さんのブログをお気に入り解除しました"
+    redirect_to blogs_url
   end
 end
